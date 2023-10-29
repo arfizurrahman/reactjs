a71d99fe9cbac9e7be96568c6be7156ce227ab87
Mohammad Arfizur Rahman <arahman@numberskills.se>
2023-10-29 10:31:46 +0600

    Work with props

--- react-18-practice/01-react-essentials-app/src/App.jsx
+++ react-18-practice/01-react-essentials-app/src/App.jsx
@@ -1,37 +1,16 @@
 import Header from "./components/Header/Header";
-import CoreConcept from "./components/CoreConcept";
-import TabButton from "./components/TabButton";
-import { CORE_CONCEPTS } from "./data";
+import CoreConcepts from "./components/CoreConcepts";
+import Examples from "./components/Examples";
 
 function App() {
-  function handleClick(selectedButton) {
-    console.log(selectedButton, "handleClick");
-  }
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Time to get started!</h2>
-          <ul>
-            {CORE_CONCEPTS.map((x) => (
-              <CoreConcept {...x} key={x.title} />
-            ))}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton onSelect={() => handleClick("components")}>
-              Components
-            </TabButton>
-            <TabButton onSelect={() => handleClick("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => handleClick("props")}>Props</TabButton>
-            <TabButton onSelect={() => handleClick("state")}>State</TabButton>
-          </menu>
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
-    </div>
+    </>
   );
 }
 
